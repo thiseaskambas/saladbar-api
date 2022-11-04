d875b1920bb2a7db85b691b181267ca1f1e6ccec
thiseaskambas <tkambas@yahoo.com>
2022-11-04 11:01:07 -0400

    merge items in cart that correspond to same product

--- src/utils/tsUtils.ts
+++ src/utils/tsUtils.ts
@@ -98,6 +98,7 @@ const isValidCartItemEntry = (item: any): item is ICartItemEntry => {
 
 const parseCartItemEntry = (item: unknown): ICartItemEntry => {
   if (!isValidCartItemEntry(item)) {
+    console.log(item);
     throw new Error('Cart item not valid');
   }
   return {
@@ -111,8 +112,36 @@ const parseCartItemsArr = (arr: unknown): ICartItemEntry[] => {
   if (!arr || !(arr instanceof Array)) {
     throw new Error('Cart items are not  inside an array');
   }
+  //NOTE: merging items in cart that correspond to same product (better implementation on the FE expected)
+  const mergedArr: any[] = arr.reduce((acc: any[], curr: any) => {
+    const search = (obj: any) => obj.product === curr.product;
+    const existIndex = acc.findIndex(search);
+    if (existIndex === -1) {
+      acc.push(curr);
+    } else {
+      const findBiggestDiscount = (accumItem: any, current: any) => {
+        let accumItemDiscount = accumItem.discount;
+        let currentItemDiscount = current.discount;
+        if (!accumItemDiscount) {
+          accumItemDiscount = 0;
+        }
+        if (!currentItemDiscount) {
+          currentItemDiscount = 0;
+        }
+        return accumItemDiscount >= currentItemDiscount
+          ? accumItemDiscount
+          : currentItemDiscount;
+      };
+      acc[existIndex] = {
+        ...acc[existIndex],
+        quantity: curr.quantity + acc[existIndex].quantity,
+        discount: findBiggestDiscount(acc[existIndex], curr),
+      };
+    }
+    return acc;
+  }, []);
 
-  return arr.map((el) => parseCartItemEntry(el));
+  return mergedArr.map((el) => parseCartItemEntry(el));
 };
 
 export const toNewCartEntry = (object: any): INewCartEntry => {
