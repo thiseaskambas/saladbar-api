561b804585df19a4b148303d25e0441008ae171b
thiseaskambas <tkambas@yahoo.com>
2022-12-21 18:00:28 -0400

    added restrictions based on user roles to patch users route

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import authController from '../controllers/authController';
 import userControllers from '../controllers/userControllers';
+import { restrictTo } from '../middleware/restrictTo';
 
 const router = Router();
 
 router.get('/', userControllers.findAllUsers);
 router.route('/update-password/').patch(authController.updatePassword);
 router.route('/:id').get(userControllers.findOneUser);
-router.route('/:id').patch(userControllers.editUser);
+router
+  .route('/:id')
+  .patch(restrictTo('admin', 'dev'), userControllers.editUser);
 
 export default router;
